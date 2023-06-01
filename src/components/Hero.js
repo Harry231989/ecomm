bef6b58eac5c032d13e254e23b4efe5556fc5181
Harry Fosu <harry23adjei@yahoo.com>
2023-06-01 17:59:16 +0200

    added alot of code

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,27 +1,34 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
-import heroBcg from '../assets/hero-bcg.jpeg'
-import heroBcg2 from '../assets/hero-bcg-2.jpeg'
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import heroBcg from '../assets/hero-bcg.jpeg';
+import heroBcg2 from '../assets/hero-bcg-2.jpeg';
 
 const Hero = () => {
-  return <Wrapper className='section-center'>
-    <article className='content'>
-      <h1>
-        design  your <br />
-        comfort zone
-      </h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, nihil cum. Eos unde quibusdam dolore doloremque quasi iusto, aspernatur voluptate itaque amet voluptatibus id. Architecto quo ab similique. Porro, labore!
-      </p>
-      <Link to='/products' className='btn hero-btn'>shop now </Link>
-    </article>
-    <article className='img-container'>
-       <img src={heroBcg} alt="nice table"  className='main-img' />
-       <img src={heroBcg2} alt="person working"  className='accent-img' />
-    </article>
-  </Wrapper>
-}
+  return (
+    <Wrapper className='section-center'>
+      <article className='content'>
+        <h1>
+          design your <br />
+          comfort zone
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, nihil
+          cum. Eos unde quibusdam dolore doloremque quasi iusto, aspernatur
+          voluptate itaque amet voluptatibus id. Architecto quo ab similique.
+          Porro, labore!
+        </p>
+        <Link to='/products' className='btn hero-btn'>
+          shop now
+        </Link>
+      </article>
+      <article className='img-container'>
+        <img src={heroBcg} alt='nice table' className='main-img' />
+        <img src={heroBcg2} alt='person working' className='accent-img' />
+      </article>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
   min-height: 60vh;
@@ -83,6 +90,6 @@ const Wrapper = styled.section`
       border-radius: var(--radius);
     }
   }
-`
+`;
 
-export default Hero
+export default Hero;
