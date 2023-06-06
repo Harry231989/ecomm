cf152687915cf307bb4b22fac55d6a6d2df97130
Harry Fosu <harry23adjei@yahoo.com>
2023-06-07 00:01:55 +0200

    Update files

--- src/index.js
+++ src/index.js
@@ -21,16 +21,18 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 //Client ID
 //11FLK4HBO8U1CaUrPlHjQRiZ06VozZFy
+//  authorizationParams={{
+//       redirect_uri: window.location.origin,
+//     }}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <Auth0Provider
-    domain='dev-tzcg6t2omgvluxty.eu.auth0.com'
-    clientId='11FLK4HBO8U1CaUrPlHjQRiZ06VozZFy'
-    authorizationParams={{
-      redirect_uri: window.location.origin,
-    }}
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+    redirectUri={window.location.origin}
+    cacheLocation='localstorage'
   >
     <UserProvider>
       <ProductsProvider>
